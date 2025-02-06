db7d3c21b4f32d83abbf279399faab4725248d46
Student <student@V314-ENT-009.local>
2025-02-06 15:54:31 -0500

    Added Movement

---
+++ Assets/movement.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public int speed = 5;
+    public Rigidbody2D rb;
+    public Transform transform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+        transform = this.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector2 direction = new Vector2(horizontal, vertical);
+        transform.Translate(direction * speed * Time.deltaTime);
+
+    }
+}
